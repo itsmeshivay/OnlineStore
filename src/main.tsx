a235e5e9303b4f40e47f2f4dc4a8849f62e01204
itsmeshivay0 <h80272655@gmail.com>
2026-03-26 12:19:06 +0530

    fresh start

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { CartProvider } from "./context/CartContext";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <CartProvider>
+    <App />
+  </CartProvider>
+);
